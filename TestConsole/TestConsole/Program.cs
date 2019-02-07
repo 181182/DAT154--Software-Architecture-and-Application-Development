f24477e0a9b349a0e92f846dd2c7bda964f6a908
Magnus Ødegård Bergersen <181182@stud.hvl.no>
2019-02-07 19:33:49 +0100

    Add files via upload
    Prosjekt fra andre obligatoriske innlevering

---
+++ TestConsole/TestConsole/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using TestDoc;
+
+namespace TestConsole
+{
+	class Program
+	{
+		public static void Main()
+		{
+			ArrayList a = new ArrayList();
+			ArrayList planets = new ArrayList();
+			ArrayList Earthmoons = new ArrayList();
+			ArrayList Marsmoons = new ArrayList();
+			ArrayList Jupitermoons = new ArrayList();
+			ArrayList Saturnmoons = new ArrayList();
+			ArrayList Uranusmoons = new ArrayList();
+			ArrayList Neptunemoons = new ArrayList();
+			ArrayList Plutomoons = new ArrayList();
+
+
+
+
+
+			using (var reader = new StreamReader(@"Z:\deadmaggi On My Mac\Dat154\Planeter.csv"))
+			{
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					var values = line.Split(';');
+					if (values[1].Equals("None"))
+					{
+						a.Add(new Star(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+					}
+					if (values[1].Equals("Sun"))
+					{
+						a.Add(new Planet(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+						planets.Add(new Planet(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+					}
+					else
+					{
+						switch (values[1])
+						{
+							case "Earth":
+								Earthmoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+							case "Mars":
+								Marsmoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+							case "Jupiter":
+								Jupitermoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+							case "Saturn":
+								Saturnmoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+							case "Uranus":
+								Uranusmoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+							case "Neptune":
+								Neptunemoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+							case "Pluto":
+								Plutomoons.Add(new Moon(values[0], Convert.ToDouble(values[2]), Convert.ToDouble(values[3]), Convert.ToDouble(values[4]), Convert.ToDouble(values[5]), "red"));
+								break;
+						}
+					}
+
+				}
+			}
+
+			for (int i = 0; i < a.Count; i++)
+			{
+				SpaceObject obj = (SpaceObject)(a[i]);
+				if (obj.name.Equals("Sun"))
+				{
+					obj.planets = planets;
+					goto ferdig;
+				}
+
+			}
+			ferdig:
+
+			for (int i = 0; i < a.Count; i++)
+			{
+				SpaceObject obj = (SpaceObject)(a[i]);
+				switch (obj.name)
+				{
+					case "Earth":
+						obj.moons = Earthmoons;
+						break;
+					case "Mars":
+						obj.moons = Marsmoons;
+						break;
+					case "Jupiter":
+						obj.moons = Jupitermoons;
+						break;
+					case "Saturn":
+						obj.moons = Saturnmoons;
+						break;
+					case "Uranus":
+						obj.moons = Uranusmoons;
+						break;
+					case "Neptune":
+						obj.moons = Neptunemoons;
+						break;
+					case "Pluto":
+						obj.moons = Plutomoons;
+						break;
+				}
+
+			}
+			Console.WriteLine("Skriv inn ønsket planet");
+			string input = Console.ReadLine();
+			Console.WriteLine("Skriv inn ønsket tid");
+			int tid = Convert.ToInt32(Console.ReadLine());
+
+			for (int i = 0; i < a.Count; i++)
+			{
+				SpaceObject obj = (SpaceObject)(a[i]);
+				if (obj.name.Equals(input) && input != "Sun")
+				{
+					{
+						obj.Draw();
+						obj.Position(tid);
+						ArrayList moons = obj.moons;
+						for (int j = 0; j < moons.Count; j++)
+						{
+							SpaceObject moon = (SpaceObject)(moons[j]);
+							moon.Draw();
+							moon.Position(tid);
+						}
+						goto ferdig2;
+					}
+				}
+				if (obj.name.Equals("Sun") && input.Equals("") || obj.name.Equals("Sun") && input.Equals("Sun"))
+				{
+					{
+						obj.Draw();
+						obj.Position(tid);
+						ArrayList planeter = obj.planets;
+						for (int j = 0; j < planeter.Count; j++)
+						{
+							SpaceObject planet = (SpaceObject)(planeter[j]); planet.Draw(); planet.Position(tid);
+						}
+						goto ferdig2;
+					}
+				}
+			}
+
+			ferdig2:
+			
+			Console.ReadLine();
+		}
+	}
+}
---
+++ TestConsole/TestConsole/spaceobj.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDoc
+{
+
+    public class SpaceObject
+    {
+
+        public String name { get; set; }
+        public double orbital_radius { get; set; }
+        public double orbital_period { get; set; }
+        public double object_radius { get; set; }
+        public double rotational_period { get; set; }
+        public String color { get; set; }
+        public ArrayList planets { get; internal set; }
+        public ArrayList moons { get; internal set; }
+
+        public SpaceObject(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color)
+        {
+            name = _name;
+            orbital_radius = _orbital_radius;
+            orbital_period = _orbital_period;
+            object_radius = _object_radius;
+            rotational_period = _rotational_period;
+            color = _color;
+        }
+
+        public virtual void Position(double time)
+        {
+            if (this.orbital_period != 0)
+            {
+                double degreeRAD = (((360 / this.orbital_period) * time) * Math.PI) / 180;
+                double x = this.orbital_radius * Math.Cos(degreeRAD);
+                double y = this.orbital_radius * Math.Sin(degreeRAD);
+
+
+                Console.WriteLine("position is : " + x + "," + y + " degrees");
+            }
+            else Console.WriteLine("position is: 0,0");
+
+        }
+        public virtual void Draw()
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    public class Star : SpaceObject
+    {
+        public Star(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Star : ");
+            base.Draw();
+        }
+        public ArrayList planets { get; set; }
+
+    }
+
+    public class Planet : SpaceObject
+    {
+        public Planet(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Planet: ");
+            base.Draw();
+        }
+        //private Moon[] moons = new Moon[30];
+        public ArrayList moons { get; set; }
+
+    }
+    public class Moon : SpaceObject
+    {
+        public Moon(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Moon : ");
+            base.Draw();
+        }
+    }
+    public class Dwarf_Planet : Planet
+    {
+        public Dwarf_Planet(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Dwarf Planet: ");
+            base.Draw();
+        }
+    }
+    public class Asteroid : SpaceObject
+    {
+        public Asteroid(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Asteroids : ");
+            base.Draw();
+        }
+    }
+    public class Asteroid_belt : Asteroid
+    {
+        public Asteroid_belt(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Asteroid belt : ");
+            base.Draw();
+        }
+    }
+    public class Comet : SpaceObject
+    {
+        public Comet(String _name, double _orbital_radius, double _orbital_period, double _object_radius, double _rotational_period, String _color) : base(_name, _orbital_radius, _orbital_radius, _object_radius, _rotational_period, _color) { }
+
+        public override void Position(double time)
+        {
+            base.Position(time);
+        }
+        public override void Draw()
+        {
+            Console.Write("Comet : ");
+            base.Draw();
+        }
+    }
+
+
+
+}
